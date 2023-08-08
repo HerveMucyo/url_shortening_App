d082d16dd3f7058623667b4cf901dc7feae14692
Herve Mucyo <mucyoh32@gmail.com>
2023-08-08 23:39:51 +0200

    Edit index.html, code.js

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql");
+const shortid = require("shortid");
 
 const app = express();
 
@@ -16,15 +17,17 @@ con.connect(function(error) {
     if (error) {
         console.log("Database connection failed");
     }
-})
+});
 
 app.get("/", function(request, response) {
     response.sendFile(__dirname + "/public/index.html");
 });
 
+// Handle form submission to create a short URL
 app.post("/api/create-short-url", function(request, response) {
-    let uniqueID = Math.random().toString(36).replace(/[^a-z0-9]/gi, '').substr(2, 10);
-    let sql = `INSERT INTO links(longurl,shorturlid) VALUES('${request.body.longurl}','${uniqueID}')`;
+    let longUrl = request.body.longurl;
+    let uniqueID = shortid.generate();
+    let sql = `INSERT INTO links(longurl, shorturlid) VALUES ('${longUrl}', '${uniqueID}')`;
     con.query(sql, function(error, result) {
         if (error) {
             response.status(500).json({
@@ -37,46 +40,22 @@ app.post("/api/create-short-url", function(request, response) {
                 shorturlid: uniqueID
             });
         }
-    })
+    });
 });
 
-app.get("/api/get-all-short-urls", function(request, response) {
-    let sql = `SELECT * FROM links`;
-    con.query(sql, function(error, result) {
-        if (error) {
-            response.status(500).json({
-                status: "notok",
-                message: "Something went wrong"
-            });
-        } else {
-            response.status(200).json(result);
-        }
-    })
-});
-
+// Handle redirection based on short URL
 app.get("/:shorturlid", function(request, response) {
     let shorturlid = request.params.shorturlid;
     let sql = `SELECT * FROM links WHERE shorturlid='${shorturlid}' LIMIT 1`;
     con.query(sql, function(error, result) {
-        if (error) {
-            response.status(500).json({
-                status: "notok",
-                message: "Something went wrong"
-            });
+        if (error || result.length === 0) {
+            response.status(404).send("Short URL not found");
         } else {
-            sql = `UPDATE links SET count=${result[0].count+1} WHERE id='${result[0].id}' LIMIT 1`;
-            con.query(sql, function(error, result2) {
-                if (error) {
-                    response.status(500).json({
-                        status: "notok",
-                        message: "Something went wrong"
-                    });
-                } else {
-                    response.redirect(result[0].longurl);
-                }
-            })
+            response.redirect(result[0].longurl);
         }
-    })
+    });
 });
 
-app.listen(5000);
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000");
+});
