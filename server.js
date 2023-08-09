19c79870f27d03d384ad08f36147b37f3aa1824b
Herve Mucyo <mucyoh32@gmail.com>
2023-08-09 17:47:51 +0200

    4

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = 3000;
-
+const port = 3001;
+const path = require('path');
 app.use(express.json());
+
+app.set('view engine', 'ejs')
+app.get('/', async(req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
+})
 
 app.post("/shorten", async(req, res) => {
     const longUrl = req.body.long_url;
     const apiKey = "dad69e0ef323fa0781f3c82d2ab198c694ea1787";
     const apiUrl = "https://api-ssl.bitly.com/v4/shorten";
+
+    // app.get('/', function(req, res) {
+    //     res.send('Hello World');
+    // });
+
+    // app.get('/', function(req, res) {
+    //     res.render('index.html');
+    // });
 
     try {
         const response = await axios.post(apiUrl, {
